070814b22686646ab0131d0003a69e6c444ea875
Elena <elenazhilina@yandex.ru>
2024-02-27 11:47:33 +0600

    Initial commit

---
+++ src/main/java/ru/netology/Maven_Task1/BonusService.java
@@ -0,0 +1,4 @@
+package ru.netology.Maven_Task1;
+
+public class BonusService {
+}
---
+++ src/main/java/ru/netology/Maven_Task1/Main.java
@@ -0,0 +1,7 @@
+package ru.netology.Maven_Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
